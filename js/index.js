cc568f4dbaf27511d1235132630dec9245daeaa0
Quibbly <owenhedwards@gmail.com>
2019-10-15 22:02:18 +0100

    Added up arrow, finished links and link hover effects

--- js/index.js
+++ js/index.js
@@ -18,10 +18,34 @@ const copyToClipboard = str => {
     }
 };
 
-
-const emailLink = document.querySelectorAll("#email");
+// Changes text upon to indicate it has been copied
+const emailLink = document.querySelectorAll("#navEmail");
 for(var i=0;i<emailLink.length;i++){
     emailLink[i].addEventListener("click", function() {
         copyToClipboard("owenhedwards@gmail.com");
+        document.getElementById("toolTipText").innerHTML = "Email address has been copied to your clipboard.";
     })
 }
+
+// Changes text back to default when user moves mouse away
+for(var i=0;i<emailLink.length;i++){
+    emailLink[i].addEventListener("mouseout", function() {
+        document.getElementById("toolTipText").innerHTML = "Click to copy address to clipboard.";
+    })
+}
+
+
+const upArrow = document.getElementById("arrowCircle");
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+}
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
